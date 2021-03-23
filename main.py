81371ecd9ec1b699bab72545022e0520267baf77
fcg299 <37026934+fcg299@users.noreply.github.com>
2021-03-23 21:58:54 +0100

    Terminado

--- main.py
+++ main.py
@@ -1,1 +1,63 @@
-print("Hola Mundo");
+polinomioUno = []
+polinomioDos = []
+resultado = []
+
+def suma():
+    i=0
+    while i<=int(Grado):
+        resultado.append(polinomioUno[i]+polinomioDos[i])
+        i += 1
+    print(f"Has invocado la suma, cuyo resultado es {resultado}")
+
+def resta():
+    i=0
+    while i<=int(Grado):
+        resultado.append(polinomioUno[i]-polinomioDos[i])
+        i += 1
+    print(f"Has invocado la resta, cuyo resultado es {resultado}")
+
+
+def multiplicacion():
+    i=0
+    while i<=int(Grado):
+        resultado.append(polinomioUno[i]*polinomioDos[i])
+        i += 1
+    print(f"Has invocado la multiplicacion, cuyo resultado es {resultado}")
+
+
+
+print("¿Hasta que grado van a llegar los polinomios?")
+Grado = int(input())
+
+print("--Rellenando los valores del primer polinomio--")
+i=0
+aux=0
+while i<=Grado:
+    print(f"Introduce el valor para el grado {Grado - i}")
+    polinomioUno.append(int(input()))
+    i += 1
+
+
+print("--Rellenando los valores del segundo polinomio--")
+i=0
+while i<=Grado:
+    print(f"Introduce el valor para el grado {Grado - i}")
+    polinomioDos.append(int(input()))
+    i += 1
+
+
+print("¿Que quieres hacer con los polinomios? \n 0 = Sumarlos \n 1 = Restar al primero el segundo \n 2 = Multiplicar")
+cuenta=int(input())
+
+if cuenta==0:
+    suma()
+if cuenta==1:
+    resta()
+if cuenta==2:
+    multiplicacion()
+
+
+
+
+
+
